e7f1b82a5aa64803d9c18163f8400030e36877b2
Tien Nguyen <tnguyen@customink.com>
2013-08-10 17:29:45 -0400

    Add World and Player skeleton files

---
+++ test/unit/player_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+require 'arkham/player'
+
+describe Player do
+  describe "an instance of Player" do
+    let(:player) { Player.new }
+  end
+end
---
+++ test/unit/world_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+require 'arkham/world'
+
+describe World do
+  describe "an instance of World" do
+    let(:world) { World.new }
+  end
+end
